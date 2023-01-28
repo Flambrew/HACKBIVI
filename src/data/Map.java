a3013bd417a71c42c93c1a2e73fd596342020ac7
Flambrew <andrew.matherne24@bishopireton.org>
2023-01-28 11:47:49 -0500

    workin on map

--- src/data/Map.java
+++ src/data/Map.java
@@ -1,5 +1,18 @@
 package src.data;
 
+import java.util.ArrayList;
+
 public class Map {
-    
+    public final String NAME;
+    private ArrayList<Location> locations;
+    private ArrayList<Connection> connections;
+
+    public Map(String name) {
+        this.NAME = name;
+    }
+
+    public void addLocation(Location loc) {
+
+    }
+
 }
