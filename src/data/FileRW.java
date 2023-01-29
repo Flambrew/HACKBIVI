eb21358e27614230686d77d11f7fcd56b25e97d9
richie-a20 <richie.almon24@bishopireton.org>
2023-01-29 00:30:39 -0500

    boink

--- src/data/FileRW.java
+++ src/data/FileRW.java
@@ -4,16 +4,17 @@ import java.io.*;
 
 public class FileRW {
 
-    //private String fName = "Map.txt";
-    //private String directory = System.getProperty("user.home");
-    //private String absolutePath = directory + File.separator + fName;
+    // private String fName = "Map.txt";
+    // private String directory = System.getProperty("user.home");
+    // private String absolutePath = directory + File.separator + fName;
 
     private static String activeFile;
-    
+    File dir = new File("C:\\Users\\camde\\OneDrive\\Desktop\\Hack BI\\HACKBIVI\\maps");
+
     public static String getActiveFile() {
         return activeFile;
     }
-    
+
     public static void setActiveFile(String activeFile) {
         FileRW.activeFile = activeFile;
     }
@@ -28,31 +29,33 @@ public class FileRW {
     }
 
     public static void transReads() {
-        
+
         try {
-            
+
             FileReader fr = new FileReader("Map.txt");
             int ch = fr.read();
-            if(ch != -1) {
-                System.out.print((char)ch);
+            if (ch != -1) {
+                System.out.print((char) ch);
             }
             fr.close();
-        } 
-        catch (FileNotFoundException e) {} 
-        catch (IOException e) {} 
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
     }
 
-    public static void transAdds() {
+    public static void transAdds(String in, Object... args) {
+        in = String.format(in, args);
+        // TODO tack . in . onto the file
         try {
-            FileWriter fw = new FileWriter("Map.txt", true);
+            FileWriter fw = new FileWriter(activeFile, true);
             BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw); {
+            PrintWriter pw = new PrintWriter(bw);
+            {
 
                 pw.println("check this out");
             }
             pw.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -60,38 +63,37 @@ public class FileRW {
     public static void transBGones() throws IOException {
 
         PrintWriter pw = new PrintWriter("newMap.txt");
-        BufferedReader br1 = new BufferedReader(new FileReader("Map.txt")); //i used two bufferedreaders for this
-          
-        String l1 = br1.readLine(); //the l's mean lines
-          
-        while(l1 != null)
-        {
+        BufferedReader br1 = new BufferedReader(new FileReader("Map.txt")); // i used two bufferedreaders for this
+
+        String l1 = br1.readLine(); // the l's mean lines
+
+        while (l1 != null) {
             boolean flag = false;
 
             BufferedReader br2 = new BufferedReader(new FileReader("delete.txt"));
-              
+
             String l2 = br2.readLine();
 
-            while(l2 != null) {
+            while (l2 != null) {
 
-                if(l1.equals(l2)) {
+                if (l1.equals(l2)) {
                     flag = true;
                     break;
-                } 
+                }
                 l2 = br2.readLine();
                 br2.close();
             }
 
-            if(!flag) 
-                pw.println(l1); 
-            l1 = br1.readLine();      
+            if (!flag)
+                pw.println(l1);
+            l1 = br1.readLine();
         }
-          
+
         pw.flush();
 
         br1.close();
         pw.close();
-          
+
         System.out.println("File operation performed successfully");
     }
 
@@ -103,6 +105,7 @@ public class FileRW {
                 System.out.println(l);
             }
             br.close();
-        } catch (IOException e) {}
-     }
+        } catch (IOException e) {
+        }
+    }
 }
