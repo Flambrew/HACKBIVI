feda9c0cf61af27d486cc701bf1216f7392bfaad
Flambrew <andrew.matherne24@bishopireton.org>
2023-01-28 19:18:35 -0500

    poop

--- src/routing/AStar.java
+++ src/routing/AStar.java
@@ -1,16 +1,40 @@
 package src.routing;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
 public class AStar {
     public static void Astar(Map map, Location start, Location end) {
         int[] dists = new int[map.getNames().size()]
         for (int i : dists) 
             i = Integer.MAX_VALUE;
 
-        while ( )
+        
 
     }
 
-    public ArrayList<Integer> connections(double[][] matrix, int)
+
+    public void calculateShortestPath(Node source) {
+        source.setDistance(0);
+        Set<Node> settlesNodes = new HashSet<>();
+        Queue<Node> unsettledNodes = new PriorityQueue<>(Collections.singleton(source));
+        while (!unsettledNodes.isEmpty()) {
+            Node currentNode = unsettledNodes.poll();
+            currentNode.getAdjacentNodes().entrySet().stream().filter(entry -> !settledNodes.contains(entry.getKey())).forEach(entry -> {
+                evaluateDistanceAndPath
+            });
+        }
+    }
+
+
+
+
+
+
 
     public double findPotential(double x, double y, double ex, double ey) {
         return Math.sqrt(Math.pow(x - ex, 2) + Math.pow(y - ey, 2));
